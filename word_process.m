3b32b95329829d30564f13b07ee3e6cd912b7729
zsc <shuchang.zhou@gmail.com>
2013-10-08 11:28:41 +0800

    Create word_process.m
    Parse a file with label as first field, and quoted text as second field.

---
+++ word_process.m
@@ -0,0 +1,7 @@
+fname="~/abe_temp_data/sentiment/sentiment.csv";ofname="~/t.csv";l=Import@fname;
+(*Drop the quotes at start and end.*)
+vec=Function[ln,{Boole[ln[[1]]=="pos"],Tally@StringSplit[StringTake[ln[[2]],{3,-3}]," "]}]/@l[[;;]];
+words=Union@(First/@(Join@@vec[[;;,2]]));dict=Dispatch@Thread[words->Range@Length@words];
+Import@Export[ofname,Prepend[Normal@SparseArray[Rule@@@(#[[2]]/.dict),Length@words],#[[1]]]&/@vec]
+(*Import@Export["~/t2.txt",Function[ln,ToString[ToString[#[[1]]]<>" "<>ToString[#[[2]]]&/@SortBy[Prepend[ln[[2]]/.dict,{0,If[ln[[1]]==0,"neg","pos"]}],First]]]/@vec[[;;]]]*)
+(*Export["t.txt","@attribute word"<>ToString@#<>" numeric"&/@Range@Length@words]*)
