ad76c9a6138db63a5323082180cadfbfe916b076
zsc <shuchang.zhou@gmail.com>
2013-11-10 17:59:59 -0800

    Update sudoku.m

--- mma/sudoku.m
+++ mma/sudoku.m
@@ -1,3 +1,6 @@
+http://rosettacode.org/wiki/Sudoku#Mathematica
+
+
 (*index=(#[[1]]-1) n+#[[2]]&;
 uncertainty=Function[{assign,k},-Length@Union[Select[neighborSets@@k,s[[#[[1]],#[[2]]]]!=0&]]];
 (*MatrixForm/@{s,next,Map[uncertainty,MapIndexed[#2&,s,{2}],{2}]}*)
@@ -53,3 +56,28 @@ explode=Function[m,With[{lengths=Union[Length/@(Join@@m)]},
 			Position[init,x_/;Length[x]==lengths[[2]]],2]]]
 	]];
 (*MatrixForm/@Nest[Join@@(explode/@#)&,{init},2]*)
+
+
+
+Needs["Combinatorica`"]
+n=9;n2=Sqrt@n;
+strs={".685...2..9.72......2983...8...32..1.14...69.6..49...8...3692......57.6..7...895."
+        ,"4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......"};
+s=ToExpression@Partition[StringSplit[StringReplace[strs[[1]],"."->"0"], ""],n];
+index=(#[[1]]-1) n+#[[2]]&;
+neighborSets=Function[{i,j}
+        ,Complement[#,{{i,j}}]&/@{Table[{i,k},{k,n}],Table[{k,j},{k,n}]
+                        ,(Join@@Table[{ii,jj}+n2{Quotient[i-1,n2],Quotient[j-1,n2]},{ii,n2},{jj,n2}])}];
+constraints=(DeleteDuplicates@Flatten@Table[If[index[#]<index@{i,j},{UndirectedEdge[index@{i,j},index@#]},{}]&/@
+	Join@@(neighborSets[i,j]),{i,n},{j,n}])/.UndirectedEdge->List;
+colornodes=Select[Tuples[Range[101,109],2],#[[1]]!=#[[2]]&];
+preassigned=Flatten[MapIndexed[If[#!=0,Function[c,{index@#2,c}]/@Complement[Range[101,109],{100+#}],{}]&,s,{2}],2];
+g=FromUnorderedPairs[Join@@{constraints,colornodes,preassigned}];
+g
+Partition[VertexColoring[g,Algorithm->Optimum],n]//MatrixForm
+
+
+
+
+Clear[a];as=Array[a,Dimensions@s];b=MapThread[If[#==0,#2,#]&,{s,as},2];
+FindInstance[And[And@@(1<=#<=n&/@Flatten@as),And@@(Flatten@Table[b[[#[[1]],#[[2]]]]!=b[[i,j]]&/@Join@@neighborSets[i,j],{i,n},{j,n}])],Flatten@as,Integers,1]
